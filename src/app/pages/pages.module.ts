ccda555ac61661911781f200b091f97b5f47d980
Silvia Quiroga <silviquir6@gmail.com>
2018-10-11 12:18:27 -0500

    agregamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import { PagesComponent } from './pages.component';
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.routes';
+
+@NgModule({
+
+declarations: [
+  DashboardComponent,
+    ProgressComponent,
+    PagesComponent,
+    Graficas1Component
+],
+exports: [
+  DashboardComponent,
+  ProgressComponent,
+  PagesComponent,
+  Graficas1Component
+],
+imports: [
+  SharedModule, PAGES_ROUTES
+]
+
+})
+
+export class PagesModule {}
